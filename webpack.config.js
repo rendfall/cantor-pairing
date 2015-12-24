a771982652c1f1803696b1caebc627edd0b21db4
rendfall <rendfall@gmail.com>
2015-12-24 23:02:59 +0100

    enviroment + algorithm + demo

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: './index.js',
+
+    output: {
+        library: 'CantorPairing',
+        libraryTarget: 'umd',
+        filename: 'CantorPairing.js',
+        path: './dist/'
+    }
+};
